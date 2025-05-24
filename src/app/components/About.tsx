f29b11cce61bcf42e46a1704c905e3a65118e8fa
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-05-24 23:17:05 +0000

    feat: ...

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
         {/* Quote block with pseudo elements */}
         <div className="relative text-md md:text-lg lg:text-xl text-gray-700 leading-relaxed px-4 md:px-10 quote-block">
           <p className="mb-6">
-            I’m Michael Yeng, the Hall President of Evandy and a dedicated student leader at the University of Ghana. Aspiring to be the next SRC President, my vision is to create a university where every student is empowered, supported, and given the opportunities to thrive.
+            I’m Michael Yeng, the Hall President of Evandy and a dedicated student leader at the University of Ghana. Aspiring to be the next SRC President, my vision is to create a university where every student is empowered, supported, and given the opportunities to thrive...
           </p>
           <p>
             To achieve this, I’ll really need your support. Let’s build the UGSRC we all want to see!
